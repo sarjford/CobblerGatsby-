ce2f003c790f7b6717e9700860edf538dc1c49dc
Sarah Ford <sarah.jeong.ford@gmail.com>
2019-08-27 18:40:28 -0700

    replace favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,9 +2,9 @@ var proxy = require("http-proxy-middleware");
 
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Cobbler Concierge 2.0`,
+    description: `Tamara Mellon microsite offering cobbler services for purchased shoes.`,
+    author: `@sarjford`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -25,10 +25,10 @@ module.exports = {
         name: `gatsby-starter-default`,
         short_name: `starter`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#fff`,
+        theme_color: `#fff`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/favicon-16x16.png`,
       },
     },
     {
